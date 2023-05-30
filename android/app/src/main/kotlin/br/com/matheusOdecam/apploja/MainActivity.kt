a81fced162a099cc895ed196c65ef96061b55ded
Matheus D. Mesquita <matheusodecam@gmail.com>
2023-05-30 13:43:10 -0300

    Init

---
+++ android/app/src/main/kotlin/br/com/matheusOdecam/apploja/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.matheusOdecam.apploja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:apploja/pages/products_overview_page.dart';
+import 'package:apploja/utils/app_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'models/cart.dart';
+import 'models/order_list.dart';
+import 'models/product_list.dart';
+import 'pages/cart_page.dart';
+import 'pages/orders_page.dart';
+import 'pages/product_detail_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => OrderList(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato',
+        ),
+        // home: const ProductsOverviewPage(),
+        routes: {
+          AppRoutes.home: (ctx) => const ProductsOverviewPage(),
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+          AppRoutes.cart: (ctx) => const CartPage(),
+          AppRoutes.orders: (ctx) => const OrdersPage(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
